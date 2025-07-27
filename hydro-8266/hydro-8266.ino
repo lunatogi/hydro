adb669163e4a98438003c4383da4f7e986aecead
Murat Utku Keti <ketomurat37@gmail.com>
2025-07-27 23:52:56 +0300

    Web server initialization

--- hydro-8266/hydro-8266.ino
+++ hydro-8266/hydro-8266.ino
@@ -1,11 +1,21 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>     
+
 #define CLK_PIN D7
 #define DATA_PIN D6
 
+//WiFi Config
+const char* ssid = "KET0";
+const char* password = "keto4522";
+
+ESP8266WebServer server(80);    // port num 80
+
+bool clk = 0;
 bool prevClk = 0;
+bool dataFull = false;
 
 uint32_t data = 0;
 uint32_t bitCounter = 0;
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,18 +25,67 @@ void setup() {
 
   pinMode(CLK_PIN, INPUT);
   pinMode(DATA_PIN, INPUT);
+
+  initWebServer();
 }
 
 void loop() {
+  server.handleClient();
+}
 
-  listenClk();
+void initWebServer(){
+  //Web Server
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(WiFi.status());
+    delay(500);
+    //Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP Started");
+}
 
 
+
+void handle_OnConnect() {
+
+  float temp = 27.6;
+  float hum = 11.2;
+
+  // Generate HTML content
+  String html = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+  html += "<!DOCTYPE html><html><head><title>Sensor Data</title></head><body>";
+  html += "<h1> Sensor Readings</h1>";
+  html += "<p>Temperature: " + String(temp) + " °C</p>";
+  html += "<p>Humidity: " + String(hum) + " %</p>";
+  html += "</body></html>";
+
+  server.send(200, "text/html", html); 
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+void sendData(){
+  pinMode(DATA_PIN, OUTPUT);
+  delay(5);
+  digitalWrite(DATA_PIN, HIGH);
+  
+  delay(5);
+  dataFull = false;
 }
 
 void listenClk(){
-  bool clk = digitalRead(CLK_PIN);
-  
   if(clk && !prevClk){
     bitCounter++;
     bool takenData = digitalRead(DATA_PIN);
@@ -39,7 +98,7 @@ void listenClk(){
       Serial.println(exactData);
       data = 0;
       bitCounter = 0;
-
+      dataFull = true;
     }
 
   }
