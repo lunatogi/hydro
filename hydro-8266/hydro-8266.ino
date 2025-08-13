1b8241838fc0c42ba9b428b8e728cff830737818
Murat Utku Keti(PC) <ketomurat37@gmail.com>
2025-08-13 18:13:58 +0300

    Properly sending data from UNO and receiving from ESP

--- hydro-8266/hydro-8266.ino
+++ hydro-8266/hydro-8266.ino
@@ -1,21 +1,15 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>     
-
 #define CLK_PIN D7
-#define DATA_PIN D6
-
-//WiFi Config
-const char* ssid = "KET0";
-const char* password = "keto4522";
-
-ESP8266WebServer server(80);    // port num 80
+#define RXDATA_PIN D6
+#define TXDATA_PIN D5
 
 bool clk = 0;
 bool prevClk = 0;
 bool dataFull = false;
 
-uint32_t data = 0;
-uint32_t bitCounter = 0;
+uint16_t data = 0;
+uint16_t bitCounter = 0;
+
+uint16_t maxBitPerData = 16;
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,76 +18,30 @@ void setup() {
 
 
   pinMode(CLK_PIN, INPUT);
-  pinMode(DATA_PIN, INPUT);
+  pinMode(RXDATA_PIN, INPUT);
 
-  initWebServer();
 }
 
 void loop() {
-  server.handleClient();
-}
-
-void initWebServer(){
-  //Web Server
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi...");
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.print(WiFi.status());
-    delay(500);
-    //Serial.print(".");
-  }
-  Serial.println("\nWiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
-
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
-
-  server.begin();
-  Serial.println("HTTP Started");
+  //Serial.println("CLK: ");
+  //Serial.println(clk);
+  //Serial.println("PrevCLK");
+  //Serial.println(prevClk);
+  listenUNO();
 }
 
 
 
-void handle_OnConnect() {
-
-  float temp = 27.6;
-  float hum = 11.2;
-
-  // Generate HTML content
-  String html = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-  html += "<!DOCTYPE html><html><head><title>Sensor Data</title></head><body>";
-  html += "<h1> Sensor Readings</h1>";
-  html += "<p>Temperature: " + String(temp) + " °C</p>";
-  html += "<p>Humidity: " + String(hum) + " %</p>";
-  html += "</body></html>";
-
-  server.send(200, "text/html", html); 
-}
-
-void handle_NotFound(){
-  server.send(404, "text/plain", "Not found");
-}
-
-void sendData(){
-  pinMode(DATA_PIN, OUTPUT);
-  delay(5);
-  digitalWrite(DATA_PIN, HIGH);
-  
-  delay(5);
-  dataFull = false;
-}
-
-void listenClk(){
+void listenUNO(){
+  clk = digitalRead(CLK_PIN);
   if(clk && !prevClk){
     bitCounter++;
-    bool takenData = digitalRead(DATA_PIN);
-    Serial.println(takenData);
+    bool takenData = digitalRead(RXDATA_PIN);
+    //Serial.println(takenData);
     data = (data << 1) | (takenData & 1);
-    Serial.println(data);
-    Serial.println(String("BC: ")+bitCounter);
-    if(bitCounter == 32){
+    //Serial.println(data);
+    //Serial.println(String("BC: ")+bitCounter);
+    if(bitCounter == maxBitPerData){
       float exactData = (float)data/100;
       Serial.println(exactData);
       data = 0;
--- hydro-uno/hydro-uno.ino
+++ hydro-uno/hydro-uno.ino
@@ -1,16 +1,16 @@
 // A0 -> Flammable Gas 
 // A1 -> TDS
 #define TDS_PIN A1
-#define FLG_PIN A0
+#define FLG_PIN A2
 #define PH_PIN A0
 
 #define CLK_PIN 11
-#define DATA_PIN 10
+#define TXDATA_PIN 10
 
 // Data wire is conntec to the Arduino digital pin 4
 #define ONE_WIRE_BUS 13
 
-#include "Adafruit_BMP085.h"    // BMP180 Air Pressure Sensor
+//#include "Adafruit_BMP085.h"    // BMP180 Air Pressure Sensor
 #include "DFRobot_PH.h"         // DFTRobot Analog pH Sensor 
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -21,20 +21,22 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature sensor 
 DallasTemperature sensors(&oneWire);
 
-Adafruit_BMP085 bmp;
+//Adafruit_BMP085 bmp;
 DFRobot_PH ph;
 
 float temp = 30.0;
+uint16_t pressure;
+uint16_t tds;
 
-uint32_t bitwiseAlt;
-uint32_t bitwiseTemp;
-uint32_t pressure;
+//Communication variables     (bitwise suffix actually means its for communication)
+uint16_t maxBitPerData = 16;
 
-uint32_t bitwisephValue;
+uint16_t bitwisephValue;
+uint16_t bitwiseAlt;
+uint16_t bitwiseTemp;
+uint16_t particleRaw;
 
-uint32_t particleRaw;
 
-uint32_t tds;
 
 void setup() {
   // put your setup code here, to run once:
@@ -44,8 +46,8 @@ void setup() {
   //}
 
   pinMode(CLK_PIN, OUTPUT);
-  pinMode(DATA_PIN, OUTPUT);
-  digitalWrite(DATA_PIN, LOW);
+  pinMode(TXDATA_PIN, OUTPUT);
+  digitalWrite(TXDATA_PIN, LOW);
   digitalWrite(CLK_PIN, LOW);
 
   sensors.begin();
@@ -54,16 +56,22 @@ void setup() {
 void loop() {         // temp/100, alt/100
   // put your main code here, to run repeatedly:
 
-  //readBMP();
+  
 
 
-  //sendBitwiseData(bitwiseTemp);
+  
 
   readOneWire();
-  
-  //readFlyingFish();
   readpH();
+
+  //digitalWrite(CLK_PIN, HIGH);
+  sendBitwiseData(bitwiseTemp);
+  delay(1);
+  sendBitwiseData(bitwisephValue);
+
+  //readBMP();
   //readTDS();
+  //readFlyingFish();
   delay(2000);
   //delay(5000);
 }
@@ -73,26 +81,28 @@ void sendBitwiseData(uint32_t data){
   Serial.println("");
   Serial.println(data);
 
-  for(int i = 31; i >= 0; i--){
+  for(int i = maxBitPerData - 1; i >= 0; i--){
     send_bit = (data >> i) & 1;
     Serial.print(send_bit);
-    digitalWrite(DATA_PIN, send_bit);
+    digitalWrite(TXDATA_PIN, send_bit);
     digitalWrite(CLK_PIN, HIGH);
     delay(1);
     digitalWrite(CLK_PIN, LOW);
     delay(1);
   }
   Serial.println("");
-  digitalWrite(DATA_PIN, LOW);
+  digitalWrite(TXDATA_PIN, LOW);
 }
 
 void readOneWire(){
   sensors.requestTemperatures(); 
   Serial.println("Temperature:");
   temp = sensors.getTempCByIndex(0) + 1.5;
+  bitwiseTemp = (float)temp * 100;
   Serial.println(temp); 
 }
 
+/*
 void readBMP(){
 
   pressure = bmp.readPressure();
@@ -114,12 +124,13 @@ void readBMP(){
 
   delay(10);
 }
+*/
 
 void readpH(){
   Serial.println("pH: ");
   float voltage = analogRead(PH_PIN)/1024.0*5000;
   float phValue = ph.readPH(voltage, temp) + 0.3;
-  bitwisephValue = phValue * 100;
+  bitwisephValue = (float)phValue * 100;
   Serial.println(phValue);
   Serial.println("---");
 
