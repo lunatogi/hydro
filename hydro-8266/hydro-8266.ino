a9d34cf76f46f688b00a0863f533b4424eb607df
Murat Utku Keti(PC) <ketomurat37@gmail.com>
2025-08-15 21:35:12 +0300

    New web socket working version with SPI connection between UNO and ESP

--- hydro-8266/hydro-8266.ino
+++ hydro-8266/hydro-8266.ino
@@ -1,128 +1,182 @@
-#define CLK_PIN D7
-#define RXDATA_PIN D6
-#define TXDATA_PIN D5
+#include "SPISlave.h"
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "LittleFS.h"
+#include <Arduino_JSON.h>
+#include <FS.h>
+//#include <Adafruit_BME280.h>
+//#include <Adafruit_Sensor.h>
 
-#define IDLE_STATE 0
-#define RECEIVE_STATE 1
-#define SEND_STATE 2
+// Replace with your network credentials
+const char* ssid = "KET0";
+const char* password = "keto4522";
 
-bool clk = 0;
-bool prevClk = 0;
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
-//Communication
-uint16_t rx_data = 0;
-uint16_t bitCounter = 0;
-uint16_t sentBitCounter = 0;
+// Create a WebSocket object
+AsyncWebSocket ws("/ws");
 
-uint16_t maxBitPerData = 16;
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
 
-bool startSend = false;
+// Timer variables
+unsigned long lastTime = 0;  
+unsigned long timerDelay = 10000;
 
-//Internal variables
-int state = 0;
-String cmd = ""         //Command from web
+// Create a sensor object
+//Adafruit_BME280 bme;         // BME280 connect to ESP32 I2C (GPIO 21 = SDA, GPIO 22 = SCL)
 
+float temp = 25.0;
+float ph = 21.1;
+float pres = 1.54;
 
+////////////////////// WEB SOCKET /////////////////////////
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+      data[len] = 0;
+      if(strcmp((char*)data, "updateSensorValue") == 0){
+        temp += 5;
+        ph += 5;
+        pres += 5;
+      }
+      String sensorReadings = getSensorReadings();
+      Serial.println(sensorReadings);
+      notifyClients(sensorReadings);
+    //}
+  }
+}
+
+void notifyClients(String sensorReadings) {
+  ws.textAll(sensorReadings);
+}
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+// Initialize LittleFS
+void initFS() {
+  if (!LittleFS.begin()) {
+    Serial.println("An error has occurred while mounting LittleFS");
+  }
+  else{
+   Serial.println("LittleFS mounted successfully");
+  }
+}
+
+void listFS(){
+  Dir dir = LittleFS.openDir("/");
+  while (dir.next()){
+    Serial.printf("FS: %s (%u bytes)\n", dir.fileName().c_str(), dir.fileSize());
+  }
+}
+
+// Get Sensor Readings and return JSON object
+String getSensorReadings(){
+  readings["temperature"] = temp;
+  readings["ph"] = ph;
+  readings["pressure"] = pres;
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+void WebSocketSetup(){
+  initWiFi();
+  initFS();
+  listFS();
+  initWebSocket();
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/index.html", "text/html");
+    //request->send(200, "text/html", "<h1>ESP8266 is alive ✅</h1>");
+  });
+
+  server.serveStatic("/", LittleFS, "/");
+
+  // Start server
+  server.begin();
+}
+/////////////////////////////////////////////////////////////////////
+
+void SPISlaveSetup(){
+  SPISlave.onData([](uint8_t *data, size_t len) {
+    char * cData = (char *)data;
+    char * rawData = cData + 1;     //Drop the first letter which says the value type
+    char index = cData[0];
+    Serial.print("Mesaj: ");
+    if(index == 't'){
+      temp = atof(rawData);
+      Serial.println(temp);
+    }else if(index == 'p'){
+      ph = atof(rawData);
+      Serial.println(ph);
+    }else{
+      String message = String(cData+1);      // So that we can get rid of 
+      Serial.println(message);
+    }
+    
+    SPISlave.setData("Ask me a question!");
+  });
+
+  SPISlave.onDataSent([]() {
+    Serial.println("Answer Sent");
+  });
+
+  SPISlave.begin();
+}
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("---ESP STARTED---");
+  Serial.println("");
+  Serial.println("Started");
 
-
-  pinMode(CLK_PIN, INPUT);
-  pinMode(RXDATA_PIN, INPUT);
-  pinMode(TXDATA_PIN, OUTPUT);
-  digitalWrite(TXDATA_PIN, LOW);
-
-  state = IDLE_STATE;
+  WebSocketSetup();
+  SPISlaveSetup();
 }
 
 void loop() {
-
-
-  switch(state){
-    case IDLE_STATE:{
-      //Serial.println("IDLE STATE");
-      cmd = Serial.readString();
-      cmd.trim();
-      if(cmd.equals("r")){
-        digitalWrite(TXDATA_PIN, HIGH);
-        startSend = true;
-        state = SEND_STATE;
-      }
-      break;
-    }
-    case RECEIVE_STATE:{
-      //Serial.println("RECEIVE STATE");
-      
-      break;
-    }
-    case SEND_STATE: {
-      //Serial.println("SEND STATE");
-      if(startSend){
-        digitalWrite(TXDATA_PIN, LOW);
-        startSend = false;
-      }else{
-        sendBitwiseData(43690);
-        
-      }
-      
-      
-      break;
-    }
+  if ((millis() - lastTime) > timerDelay) {
+    String sensorReadings = getSensorReadings();
+    Serial.print(sensorReadings);
+    notifyClients(sensorReadings);
+    lastTime = millis();
   }
 
-  
-  delay(1);
-}
-
-void sendUNO(){
-
-}
-
-void sendBitwiseData(uint16_t data){
-  bool send_bit;
-  //Serial.println("Trying to send");
-  if(sentBitCounter < maxBitPerData){
-    //Serial.println("Bit counter less than max bit");
-    clk = digitalRead(CLK_PIN);
-    //Serial.println(clk);
-    if(clk && !prevClk){                                                //Send at each posedge clk
-      Serial.println("Posedge");
-      send_bit = (data >> (maxBitPerData-(1 + sentBitCounter))) & 1;    //Starting from MSB
-      Serial.print("Sent bit: ");
-      Serial.println(send_bit);
-      Serial.println(sentBitCounter);
-      digitalWrite(TXDATA_PIN, send_bit);
-      sentBitCounter++;
-      if(sentBitCounter == maxBitPerData){
-        sentBitCounter = 0;
-        digitalWrite(TXDATA_PIN, LOW);
-      }
-    }
-    prevClk = clk;
-  }
-  
-}
-
-
-void listenUNO(){
-  clk = digitalRead(CLK_PIN);
-  if(clk && !prevClk){
-    bitCounter++;
-    bool rx_bit = digitalRead(RXDATA_PIN);
-    //Serial.println(rx_bit);
-    rx_data = (rx_data << 1) | (rx_bit & 1);
-    //Serial.println(data);
-    //Serial.println(String("BC: ")+bitCounter);
-    if(bitCounter == maxBitPerData){
-      float exactData = (float)rx_data/100;
-      Serial.println(exactData);
-      rx_data = 0;
-      bitCounter = 0;
-    }
-
-  }
-  prevClk = clk;
+  ws.cleanupClients();
 }
--- hydro-uno/hydro-uno.ino
+++ hydro-uno/hydro-uno.ino
@@ -1,24 +1,23 @@
-// A0 -> Flammable Gas 
-// A1 -> TDS
+/*          SPI CONNECTIONS
+    GPIO    NodeMCU   Name  |   Uno
+   ===================================
+     15       D8       SS   |   D10
+     13       D7      MOSI  |   D11
+     12       D6      MISO  |   D12
+     14       D5      SCK   |   D13
+*/
 #define PH_PIN A0
 #define TDS_PIN A1
 #define FLG_PIN A2
 
-#define CLK_PIN 11
-#define TXDATA_PIN 10
-#define RXDATA_PIN 9
-
-#define IDLE_STATE 0
-#define RECEIVE_STATE 1
-#define SEND_STATE 2
-
 // Data wire is conntec to the Arduino digital pin 4
-#define ONE_WIRE_BUS 13
+#define ONE_WIRE_BUS 8
 
 //#include "Adafruit_BMP085.h"    // BMP180 Air Pressure Sensor
 #include "DFRobot_PH.h"         // DFTRobot Analog pH Sensor 
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <SPI.h>
 
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
@@ -30,65 +29,101 @@ DallasTemperature sensors(&oneWire);
 DFRobot_PH ph;
 
 float temp = 30.0;
-uint16_t pressure;
-uint16_t tds;
+float phValue = 0;
+float particle = 1;
+float tds = 2;
 
-//Communication variables     (bitwise suffix actually means its for communication)
-uint16_t maxBitPerData = 16;
-uint16_t rx_data = 0;
+///////////////////// SPI ////////////////////////
+class ESPMaster {
+private:
+  uint8_t _ss_pin;
+  void _pulseSS() {
+    digitalWrite(_ss_pin, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(_ss_pin, LOW);
+  }
 
-uint16_t bitwisephValue;
-uint16_t bitwiseAlt;
-uint16_t bitwiseTemp;
-uint16_t particleRaw;
+public:
+  ESPMaster(uint8_t pin)
+    : _ss_pin(pin) {}
+  void begin() {
+    pinMode(_ss_pin, OUTPUT);
+    _pulseSS();
+  }
 
-int state = 0;
-int stateCounter = 0;
+  void readData(uint8_t *data) {
+    _pulseSS();
+    SPI.transfer(0x03);
+    SPI.transfer(0x00);
+    for (uint8_t i = 0; i < 32; i++) { data[i] = SPI.transfer(0); }
+    _pulseSS();
+  }
 
-bool startListen = false;
+  void writeData(uint8_t *data, size_t len) {
+    uint8_t i = 0;
+    _pulseSS();
+    SPI.transfer(0x02);
+    SPI.transfer(0x00);
+    while (len-- && i < 32) { SPI.transfer(data[i++]); }
+    while (i++ < 32) { SPI.transfer(0); }
+    _pulseSS();
+  }
+
+  String readData() {
+    char data[33];
+    data[32] = 0;
+    readData((uint8_t *)data);
+    return String(data);
+  }
+
+  void writeData(const char *data) {
+    writeData((uint8_t *)data, strlen(data));
+  }
+};
+
+ESPMaster esp(SS);
+
+void sendESP(const char *message) {
+  Serial.print("Master: ");
+  Serial.println(message);
+  esp.writeData(message);
+  delay(10);
+  Serial.print("Slave: ");
+  Serial.println(esp.readData());
+  Serial.println();
+}
+
+void SPIMasterSetup(){
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+  esp.begin();
+  sendESP("Hello Slave!");         // Unnecessary
+}
+///////////////////////////////////////////////////////
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  sensors.begin();    //OneWire temperature sensor
+ 	SPIMasterSetup();
+
   //if(!bmp.begin()){
   //  Serial.println("BMP180 not found!");
   //}
-
-  pinMode(CLK_PIN, OUTPUT);
-  pinMode(TXDATA_PIN, OUTPUT);
-  pinMode(RXDATA_PIN, INPUT);
-  digitalWrite(TXDATA_PIN, LOW);
-  digitalWrite(CLK_PIN, LOW);
-
-  sensors.begin();
-  state = IDLE_STATE;
 }
-
+int tt = 0;
 void loop() {         // temp/100, alt/100
   // put your main code here, to run repeatedly:
 
+  readOneWire();
+  String tempS = "t"+String(temp);
+  sendESP(tempS.c_str());
 
-  switch(state){
-    case IDLE_STATE:{
-      //Serial.println("IDLE STATE");
-      bool data = digitalRead(RXDATA_PIN);
-      if(data){
-        state = RECEIVE_STATE;
-      }
-      break;
-    }
-    case RECEIVE_STATE:{
-      //Serial.println("RECEIVE STATE");
-      listenESP();
-      break;
-    }
-    case SEND_STATE: {
-      //Serial.println("SEND STATE");
-      readOneWire();
-      sendBitwiseData(bitwiseTemp);
-      break;
-    }
-  }
+  readpH();
+  String phS = "p"+String(phValue);
+  sendESP(phS.c_str());
+  delay(1000);
+
 
 
   
@@ -115,67 +150,13 @@ void loop() {         // temp/100, alt/100
   //delay(5000);
 }
 
-void listenESP(){
-  if(startListen){                    // For it to wait ESP to come here
-    bool data = digitalRead(RXDATA_PIN);
-    if(!data) startListen = false;
-  }else{
-    for(int i = 0; i < maxBitPerData; i++){
-      digitalWrite(CLK_PIN, HIGH);
-      delay(5);
-      bool rx_bit = digitalRead(RXDATA_PIN);
-      digitalWrite(CLK_PIN, LOW);
-      delay(1);
-      Serial.print("---");
-      Serial.print("Counter: ");
-      Serial.println(i);
-      Serial.print("Read bit: ");
-      Serial.println(rx_bit);
-      rx_data = (rx_data << 1) | (rx_bit & 1);
-      if(i == maxBitPerData-1){
-        float exactData = (float)rx_data/100;
-        Serial.print("Taken Data: ");
-        Serial.println(exactData);
-        rx_data = 0;
-        state = IDLE_STATE;
-      }
-    }
-  }
-}
 
-void sendBitwiseData(uint16_t data){
-  bool send_bit;
-  Serial.println("");
-  Serial.println(data);
 
-  for(int i = maxBitPerData - 1; i >= 0; i--){
-    send_bit = (data >> i) & 1;
-    Serial.print(send_bit);
-    digitalWrite(TXDATA_PIN, send_bit);
-    digitalWrite(CLK_PIN, HIGH);
-    delay(3);
-    digitalWrite(CLK_PIN, LOW);
-    delay(3);
-  }
-  Serial.println("");
-  digitalWrite(TXDATA_PIN, LOW);
-}
-
-void stateController(int curState){
-  if(curState == RECEIVE_STATE){
-    if(stateCounter == 0){
-      if(calValue == 0xFFFF){
-        state = SEND_STATE;
-      }
-    }
-  }
-}
 
 void readOneWire(){
   sensors.requestTemperatures(); 
   Serial.println("Temperature:");
   temp = sensors.getTempCByIndex(0) + 1.5;
-  bitwiseTemp = (float)temp * 100;
   Serial.println(temp); 
 }
 
@@ -206,8 +187,7 @@ void readBMP(){
 void readpH(){
   Serial.println("pH: ");
   float voltage = analogRead(PH_PIN)/1024.0*5000;
-  float phValue = ph.readPH(voltage, temp) + 0.3;
-  bitwisephValue = (float)phValue * 100;
+  phValue = ph.readPH(voltage, temp) + 0.3;
   Serial.println(phValue);
   Serial.println("---");
 
@@ -216,8 +196,8 @@ void readpH(){
 
 void readFlyingFish(){
   Serial.println("Flammable Gas: ");
-  particleRaw = analogRead(FLG_PIN);
-  Serial.println(particleRaw);
+  particle = analogRead(FLG_PIN);
+  Serial.println(particle);
   Serial.println("---");
   Serial.println("");
 
